895d58958fd83021260c4fc549557a542229cb95
aureabelle <acruz@inboxlab.com>
2019-07-05 22:06:44 -0400

    github authorization commented out

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,39 @@
 import axios from "axios";
 
+export const FETCH_PROJECTS = "FETCH_PROJECTS";
+export const FETCH_CONTRIBUTORS = "FETCH_CONTRIBUTORS";
+
+export function fetchProjects() {
+  const url = 'https://api.github.com/orgs/facebook/repos?per_page=500';
+  const request = axios.get(url, {
+      headers: {
+        "Accept": "application/vnd.github.inertia-preview+json"
+      }
+    });
+
+  return {
+    type: FETCH_PROJECTS,
+    payload: request
+  };
+}
+
+export function fetchContributors(url) {
+  const request = axios.get(url, {
+      headers: {
+        "Accept": "application/vnd.github.inertia-preview+json"
+      }
+    });
+
+  return {
+    type: FETCH_CONTRIBUTORS,
+    payload: request
+  };
+}
+
+
+
+// With Token and Authorization
+/*
 const tokenId = '7a6a5bcb6b613cb99711aec9f9845fa358a3d068';
 
 export const FETCH_PROJECTS = "FETCH_PROJECTS";
@@ -32,4 +66,4 @@ export function fetchContributors(url) {
     type: FETCH_CONTRIBUTORS,
     payload: request
   };
-}
+}*/
