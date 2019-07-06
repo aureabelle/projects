13c700f8373e2baa078c5966a1109ca9d32edcd7
aureabelle <acruz@inboxlab.com>
2019-07-05 20:31:49 -0400

    github credentials

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const token = 'token 7a6a5bcb6b613cb99711aec9f9845fa358a3d068';
+const tokenId = '7a6a5bcb6b613cb99711aec9f9845fa358a3d068';
 
 export const FETCH_PROJECTS = "FETCH_PROJECTS";
 export const FETCH_CONTRIBUTORS = "FETCH_CONTRIBUTORS";
@@ -9,8 +9,8 @@ export function fetchProjects() {
   const url = 'https://api.github.com/orgs/facebook/repos?per_page=500';
   const request = axios.get(url, {
       headers: {
-        "Accept": "application/vnd.github.inertia-preview+json",
-        "Authorization": token
+        "Accept": "application/json",
+        "Authorization": `token ${tokenId} https://api.github.com`
       }
     });
 
@@ -23,8 +23,8 @@ export function fetchProjects() {
 export function fetchContributors(url) {
   const request = axios.get(url, {
       headers: {
-        "Accept": "application/vnd.github.inertia-preview+json",
-        "Authorization": token
+        "Accept": "application/json",
+        "Authorization": `token ${tokenId} https://api.github.com`
       }
     });
 
